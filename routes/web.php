30d6d0ad18f85aae7ddeb57e7fd97537341e26fb
lykalaik <alaikasalva8@gmail.com>
2024-04-26 14:14:31 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ReviewsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,19 +19,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Home route
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('home');
 
-Route::middleware('auth')->group(function () {
+// Menut route
+Route::get('/menu', [AboutController::class, 'index'])->name('menu');
+
+
+// About route
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+
+// Reviews route
+Route::get('/reviews', [ReviewsController::class, 'index'])->name('reviews');
+
+// Dashboard route
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // Posts resource routes
     Route::resource('posts', PostController::class);
 });
 
